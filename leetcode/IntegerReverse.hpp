15ff126b7d93d41baf820abc430439786bca7431
muzhy <904489394@qq.com>
2022-05-04 20:11:59 +0800

    添加Solution类，将测试用例与解法结合在一起

--- leetcode/IntegerReverse.hpp
+++ leetcode/IntegerReverse.hpp
@@ -1,9 +1,25 @@
+#include "Solution.hpp"
+
+#include <vector>
+#include <map>
 
 namespace leetCode
-{
+{   
     /***********************************************************************
      * 给定一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
      * 如果反转后整数超过 32 位的有符号整数的范围 [−2^31,  2^31 − 1] ，就返回 0
      ********************************************************************/
-    int IntegerReverse(int x);
+    class IntegerReverseSolution : public Solution
+    {
+    public:
+        IntegerReverseSolution();
+        virtual ~IntegerReverseSolution();
+        virtual std::string run();
+    private:
+        int integerReverse(int x);
+    private:
+        // 测试用例
+        std::vector<std::pair<int, int>> testCase;
+    };
+
 } // namespace leetCode
--- main.cpp
+++ main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 
-#include "TestDemo.hpp"
 #include "IntegerReverse.hpp"
 
 using namespace std;
 
 int main(int, char**) {
-    cout << leetCode::GetLibName() << endl;
-
-    // cout << leetCode::IntegerReverse(123) << endl;
-    // cout << leetCode::IntegerReverse(-123) << endl;
-    // cout << leetCode::IntegerReverse(120) << endl;
-    // cout << leetCode::IntegerReverse(0) << endl;
-    // cout << leetCode::IntegerReverse(1534236469) << endl;
-    cout << leetCode::IntegerReverse(-2147483648) << endl;
+    leetCode::IntegerReverseSolution solution;
+    cout << solution.run() << endl;
 }
