d35573722f103d8da81d4629d8113ade70c9e91e
muzhy <904489394@qq.com>
2022-05-04 16:50:22 +0800

    添加整数反转的解法

---
+++ leetcode/IntegerReverse.cpp
@@ -0,0 +1,23 @@
+#include "IntegerReverse.hpp"
+
+#include <climits>
+
+namespace leetCode
+{
+    using namespace std;
+
+    int IntegerReverse(int x)
+    {
+        int res = 0;
+        while(x > 0)
+        {
+             if (res < INT_MIN / 10 || res > INT_MAX / 10) {
+                return 0;
+            }
+            res = res * 10 + x % 10;
+            x /= 10;            
+        }
+        
+        return  res;
+    }
+} // namespace leetCode
--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 
 #include "TestDemo.hpp"
+#include "IntegerReverse.hpp"
 
 using namespace std;
 
 int main(int, char**) {
     cout << leetCode::GetLibName() << endl;
+
+    // cout << leetCode::IntegerReverse(123) << endl;
+    // cout << leetCode::IntegerReverse(-123) << endl;
+    // cout << leetCode::IntegerReverse(120) << endl;
+    // cout << leetCode::IntegerReverse(0) << endl;
+    // cout << leetCode::IntegerReverse(1534236469) << endl;
+    cout << leetCode::IntegerReverse(-2147483648) << endl;
 }
