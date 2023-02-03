8d8e826c674056d9749af86a5f350e6778cfdde6
zhychen <zhychen@coremail.cn>
2023-02-03 17:55:39 +0800

    线程安全队列在被销毁时发出信号，防止外部一直阻塞在pop函数上

--- dataStruct/TSQueue.hpp
+++ dataStruct/TSQueue.hpp
@@ -16,11 +16,18 @@ namespace furry_toy
     class TSQueue
     {
     public:
-        TSQueue() {}
-        TSQueue(const TSQueue& otherQueue)
+        TSQueue() : m_data(), m_dataMut(), m_dataCond(), m_destory(false) {}
+        TSQueue(const TSQueue& otherQueue) 
         {
             std::lock_guard<std::mutex> lock(otherQueue.m_dataMut);
             m_data = otherQueue.m_data;
+        }
+        ~TSQueue(){
+            {
+                std::lock_guard<std::mutex> lock(m_dataMut);
+                m_destory = true;
+            }
+            m_dataCond.notify_all();
         }
         // 禁止赋值操作
         TSQueue& operator=(const TSQueue&) = delete;
@@ -59,9 +66,13 @@ namespace furry_toy
         {
             std::unique_lock<std::mutex> lock(m_dataMut);
             if(m_dataCond.wait_for(lock, std::chrono::milliseconds(timeout), [this]{
-                return m_data.empty() == false;
-            }))
+                return m_data.empty() == false || m_destory;
+            }))            
             {
+                if(m_destory)
+                {
+                    return false;
+                }
                 value = m_data.front();
                 m_data.pop();
             }
@@ -74,9 +85,13 @@ namespace furry_toy
         {
             std::unique_lock<std::mutex> lock(m_dataMut);
             if(m_dataCond.wait_for(lock, std::chrono::milliseconds(timeout), [this]{
-                return m_data.empty() == false;
+                return m_data.empty() == false || m_destory;
             }))
             {
+                if(m_destory)
+                {
+                    return nullptr;
+                }
                 std::shared_ptr<T> res(std::make_shared<T>(m_data.front()));
                 m_data.pop();
                 return res;
@@ -87,18 +102,27 @@ namespace furry_toy
             }
         }
 
-        //若队列中没有数据，则会阻塞，直到队列中有数据再返回
-        void pop(T& value)
+        //若队列中没有数据，则会阻塞，直到队列中有数据或队列被销毁时再返回
+        // 若队列表销毁，此时pop返回false
+        bool pop(T& value)
         {
             std::unique_lock<std::mutex> lock(m_dataMut);
-            m_dataCond.wait(lock, [this]{ return !m_data.empty(); });
+            m_dataCond.wait(lock, [this]{ return !m_data.empty() || m_destory; });
+            if(m_destory)
+            {
+                return false;
+            }
             value = m_data.front();
             m_data.pop();
         }
         std::shared_ptr<T> pop()
         {
             std::unique_lock<std::mutex> lock(m_dataMut);
-            m_dataCond.wait(lock, [this]{ return !m_data.empty(); });
+            m_dataCond.wait(lock, [this]{ return !m_data.empty() || m_destory; });
+            if(m_destory)
+            {
+                return nullptr;
+            }
             std::shared_ptr<T> res(std::make_shared<T>(m_data.front()));
             m_data.pop();
             return res;
@@ -113,6 +137,7 @@ namespace furry_toy
         std::queue<T> m_data;
         mutable std::mutex m_dataMut;
         std::condition_variable m_dataCond;
+        bool m_destory;
     };
 }
 
